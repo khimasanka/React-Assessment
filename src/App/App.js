b900f9cb05ffdbdc7f04e5fa3332a69637b25896
Kaveesha Himasanka <kaveee143@gmail.com>
2022-08-05 13:17:06 +0530

    set login form

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import './App.css';
+import Login from "../pages/login/login";
 function App() {
   return (
     <div className="App">
-        <h1>hello  </h1>
+      <Login/>
     </div>
   );
 }
