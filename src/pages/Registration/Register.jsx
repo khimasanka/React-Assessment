9ab2086332b475b8c33b8c70007a7cb2a48f6130
Kaveesha Himasanka <kaveee143@gmail.com>
2022-08-06 00:48:29 +0530

    set clear fields method

--- src/pages/Registration/Register.jsx
+++ src/pages/Registration/Register.jsx
@@ -7,34 +7,35 @@ import SnackBar from "../../components/common/snackBar/SnackBar";
 import {styleSheet} from "./styles"
 import {withStyles} from "@mui/styles";
 import Container from "@mui/material/Container";
-import {CssBaseline, Paper} from "@mui/material";
+import {CssBaseline, Divider, Paper} from "@mui/material";
 import {ThemeProvider} from "@emotion/react";
 import {createTheme} from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import UserService from "../../services/UserService";
 
 class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
             formData: {
-                email:'',
-                username:'',
-                password:'',
-                name:{
-                    firstname:'',
-                    lastname:''
+                email: '',
+                username: '',
+                password: '',
+                name: {
+                    firstname: '',
+                    lastname: ''
                 },
-                address:{
-                    city:'',
-                    street:'',
-                    number:'',
-                    zipcode:'',
-                    geolocation:{
-                        lat:'',
-                        long:''
+                address: {
+                    city: '',
+                    street: '',
+                    number: '',
+                    zipcode: '',
+                    geolocation: {
+                        lat: '',
+                        long: ''
                     }
                 },
-                phone:''
+                phone: ''
             },
             alert: false,
             message: '',
@@ -44,6 +45,47 @@ class Register extends Component {
 
     submitUser = async () => {
         console.log(this.state.formData)
+        let data = this.state.formData
+        let res = await UserService.userRegister(data);
+        if (res.status === 200) {
+            this.clearFields();
+            this.setState({
+                alert: true,
+                message: 'Register Success',
+                severity: 'success'
+            });
+        } else {
+            this.setState({
+                alert: true,
+                message: 'Register Failed..! Try again',
+                severity: 'warning'
+            });
+        }
+    }
+
+    clearFields=()=>{
+        this.setState({
+            formData: {
+                email: '',
+                username: '',
+                password: '',
+                name: {
+                    firstname: '',
+                    lastname: ''
+                },
+                address: {
+                    city: '',
+                    street: '',
+                    number: '',
+                    zipcode: '',
+                    geolocation: {
+                        lat: '',
+                        long: ''
+                    }
+                },
+                phone: ''
+            }
+        });
     }
 
     render() {
@@ -56,17 +98,21 @@ class Register extends Component {
 
                     <Container component="main" maxWidth="lg" sx={{mb: 4}}>
                         <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
-                            <Typography component="h1" variant="h4" align="center">
-                               User Registration
-                            </Typography>
+                            <h1 className={"regHeader"} align="center">
+                                User Registration
+                            </h1>
 
-                            <div className={classes.container}>
+                            <div className={classes.container} style={{background:'rgba(211,229,239,0.11)'}}>
                                 <Grid container sm={8} sx={12} lg={8} md={8} mt={4}>
                                     <ValidatorForm ref="form" onSubmit={this.submitUser}
-                                                   onError={errors => console.log(errors)}>
-                                        <Typography variant="h6" gutterBottom>
-                                            User Registration
-                                        </Typography>
+                                                   onError={() => {
+                                                       this.setState({
+                                                           alert: true,
+                                                           message: 'Something went wrong..!',
+                                                           severity: 'error'
+                                                       });
+                                                   }}>
+
                                         <Grid container spacing={3}>
                                             <Grid item xs={12} md={6}>
                                                 <TextValidator
@@ -78,7 +124,7 @@ class Register extends Component {
                                                     autoComplete="given-name"
                                                     validators={['required', 'isString', 'maxStringLength:20', 'minStringLength:2']}
                                                     value={this.state.formData.name.firstname}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.name
                                                         data.firstname = e.target.value
                                                         this.setState({data});
@@ -95,7 +141,7 @@ class Register extends Component {
                                                     autoComplete="given-name"
                                                     validators={['required', 'isString', 'maxStringLength:20', 'minStringLength:2']}
                                                     value={this.state.formData.name.lastname}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.name
                                                         data.lastname = e.target.value
                                                         this.setState({data});
@@ -113,7 +159,7 @@ class Register extends Component {
                                                     validators={['required', 'isEmail']}
                                                     errorMessages={['this field is required', 'Invalid Email']}
                                                     value={this.state.formData.email}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData
                                                         data.email = e.target.value
                                                         this.setState({data});
@@ -129,7 +175,7 @@ class Register extends Component {
                                                     fullWidth
                                                     validators={['required', 'isString', 'maxStringLength:20', 'minStringLength:5']}
                                                     value={this.state.formData.username}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData
                                                         data.username = e.target.value
                                                         this.setState({data});
@@ -149,7 +195,7 @@ class Register extends Component {
                                                     validators={['required', 'isString', 'minStringLength:5']}
                                                     errorMessages={['this field is required', 'Password too short']}
                                                     value={this.state.formData.password}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData
                                                         data.password = e.target.value
                                                         this.setState({data});
@@ -161,11 +207,11 @@ class Register extends Component {
                                                     required
                                                     label="City"
                                                     fullWidth
-                                                    validators={['required', 'isString', 'minStringLength:4','maxStringLength:20']}
+                                                    validators={['required', 'isString', 'minStringLength:4', 'maxStringLength:20']}
                                                     variant="filled"
                                                     size="small"
                                                     value={this.state.formData.address.city}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.address
                                                         data.city = e.target.value
                                                         this.setState({data});
@@ -181,7 +227,7 @@ class Register extends Component {
                                                     size="small"
                                                     validators={['required', 'isString', 'maxStringLength:20', 'minStringLength:4']}
                                                     value={this.state.formData.address.street}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.address
                                                         data.street = e.target.value
                                                         this.setState({data});
@@ -197,7 +243,7 @@ class Register extends Component {
                                                     validators={['required', 'isString', 'maxStringLength:5', 'minStringLength:1']}
                                                     size="small"
                                                     value={this.state.formData.address.number}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.address
                                                         data.number = e.target.value
                                                         this.setState({data});
@@ -210,10 +256,10 @@ class Register extends Component {
                                                     required
                                                     fullWidth
                                                     variant="filled"
-                                                    validators={['required', 'isNumber','maxNumber:99999', 'matchRegexp:^[0-9]{5}$']}
+                                                    validators={['required', 'isNumber', 'maxNumber:99999', 'matchRegexp:^[0-9]{5}$']}
                                                     size="small"
                                                     value={this.state.formData.address.zipcode}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.address
                                                         data.zipcode = e.target.value
                                                         this.setState({data});
@@ -229,7 +275,7 @@ class Register extends Component {
                                                     size="small"
                                                     value={this.state.formData.address.geolocation.lat}
                                                     validators={['required', 'isFloat']}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.address.geolocation
                                                         data.lat = e.target.value
                                                         this.setState({data});
@@ -245,7 +291,7 @@ class Register extends Component {
                                                     size="small"
                                                     value={this.state.formData.address.geolocation.long}
                                                     validators={['required', 'isFloat']}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData.address.geolocation
                                                         data.long = e.target.value
                                                         this.setState({data});
@@ -263,7 +309,7 @@ class Register extends Component {
                                                     validators={['required', 'isNumber', 'matchRegexp:^0?(7)[0|1|2|4|5|6|7|8]-?[0-9]{7}$']}
                                                     errorMessages={['this field is required', 'Invalid Phone Number']}
                                                     value={this.state.formData.phone}
-                                                    onChange={(e)=>{
+                                                    onChange={(e) => {
                                                         let data = this.state.formData
                                                         data.phone = e.target.value
                                                         this.setState({data});
@@ -273,7 +319,8 @@ class Register extends Component {
 
                                         </Grid>
                                         <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
-                                            <Buttons  sx={{mt: 3, ml: 1,fontSize:15,fontWeight:"bold"}} color="secondary">
+                                            <Buttons onClick={this.clearFields} sx={{mt: 3, ml: 1, fontSize: 15, fontWeight: "bold"}}
+                                                     color="secondary">
                                                 Clear
                                             </Buttons>
                                             <button className={'saveBtn'} type="submit" style={{mt: 3, ml: 1}}>
---
+++ src/services/UserService.js
@@ -0,0 +1,18 @@
+import axios from "../axios";
+
+class UserService{
+    userRegister =async (data)=>{
+        const promise = new Promise((resolve,reject) => {
+            axios.post('users', data)
+                .then((res) => {
+                    return resolve(res);
+                })
+                .catch((err) => {
+                    return resolve(err);
+                });
+        });
+        return await promise;
+    }
+}
+
+export default new UserService();
